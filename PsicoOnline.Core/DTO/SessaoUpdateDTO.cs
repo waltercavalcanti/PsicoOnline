a571d9b65238ce31010216c2250284c4ef42f480
Walter Cavalcanti <waltercavalcanti@gmail.com>
2022-10-20 10:14:58 -0300

    Correção do DTO SessaoUpdateDTO que estava gerando falha ao atualizar uma sessão

--- PsicoOnline.Core/DTO/SessaoUpdateDTO.cs
+++ PsicoOnline.Core/DTO/SessaoUpdateDTO.cs
@@ -4,5 +4,9 @@ public class SessaoUpdateDTO
 {
     public int Id { get; set; }
 
+    public int PacienteId { get; set; }
+
+    public DateTime DataSessao { get; set; }
+
     public string Anotacao { get; set; }
 }
