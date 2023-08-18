9c24c1d27ebd6273148a3d35bb253310e99a08e2
Walter Cavalcanti <waltercavalcanti@gmail.com>
2023-08-18 11:27:43 -0300

    Altera o ícone do switch através de chamada a método.

--- PsicoOnline.UI/Shared/MainLayout.razor.cs
+++ PsicoOnline.UI/Shared/MainLayout.razor.cs
@@ -2,11 +2,21 @@ namespace PsicoOnline.UI.Shared;
 
 public partial class MainLayout
 {
-    bool gavetaAberta = true;
-    private bool modoEscuro;
+	bool gavetaAberta = true;
+	private bool modoEscuro;
+	private string iconeAtual = Icons.Material.Outlined.LightMode;
 
-    void AlternarGaveta()
-    {
-        gavetaAberta = !gavetaAberta;
-    }
+	void AlternarGaveta()
+	{
+		gavetaAberta = !gavetaAberta;
+	}
+
+	private Task AlternarModoEscuro()
+	{
+		modoEscuro = !modoEscuro;
+
+		iconeAtual = modoEscuro ? Icons.Material.Outlined.DarkMode : Icons.Material.Outlined.LightMode;
+
+		return Task.CompletedTask;
+	}
 }
